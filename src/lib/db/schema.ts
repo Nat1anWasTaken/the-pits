c4c3ed2378316ef0a2e2de112e438ecfc62f50ee
Nat1anWasTaken <nathan@nat1an.xyz>
2025-10-24 14:56:37 +0800

    chore: initialize database

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,30 @@
+import { integer, pgTable, varchar, foreignKey } from 'drizzle-orm/pg-core';
+
+export const usersTable = pgTable('users', {
+	id: integer().primaryKey().generatedAlwaysAsIdentity(),
+	name: varchar({ length: 255 }).notNull(),
+	age: integer().notNull(),
+	email: varchar({ length: 255 }).notNull().unique()
+});
+
+export const boardsTable = pgTable('boards', {
+	id: integer().primaryKey().generatedAlwaysAsIdentity(),
+	name: varchar({ length: 255 }).notNull()
+});
+
+export const cardsTable = pgTable(
+	'cards',
+	{
+		id: integer().primaryKey().generatedAlwaysAsIdentity(),
+		boardId: integer().notNull(),
+		name: varchar({ length: 255 }).notNull(),
+		status: varchar({ length: 100 }).notNull(),
+		ownerName: varchar({ length: 255 }).notNull()
+	},
+	(table) => [
+		foreignKey({
+			columns: [table.boardId],
+			foreignColumns: [boardsTable.id]
+		})
+	]
+);
